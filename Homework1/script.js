3057d446988273a7435c7a7d9dcb4684fed7214c
Jaroslavna <kostiv88@icloud.com>
2022-07-22 18:27:01 +0300

    add

---
+++ Homework1/script.js
@@ -0,0 +1,63 @@
+/*let a = "hello ";
+let b = "owu ";
+let c = "com ";
+let d = "ua ";
+let f = 1;
+let e = 10;
+let g = -999;
+let h = 123;
+const pi = 3.14;
+let i = 2.7;
+let k = 16;
+let b1 = true;
+let b2 = false;*/
+
+/*alert (a+b+c+d);
+alert(f);
+alert(e);
+alert(g);
+alert(h);
+alert(pi);
+alert(i);
+alert(k);
+alert(b1);
+alert(b2);*/
+
+/*console.log (a+b+c+d);
+console.log (f);
+console.log (e);
+console.log (g)
+console.log (h, pi, i, k, b1, b2);
+
+document.write (a+b+c+d);
+document.write (f);
+document.write (e);
+document.write (g);
+document.write (h);
+document.write (pi);
+document.write (i);
+document.write (k);
+document.write (b1);
+document.write (b2);
+
+let firstName =a= "Jaroslavna ";
+let middleName =b= "Igorivna";
+let lastName =c= "Kostiv ";
+let persone=c+a+b;
+console.log(persone);*/
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log (typeof a);
+console.log (typeof b);
+console.log (typeof c);
+
+let  firstName = prompt("Ім'я");
+console.log (firstName);
+let  middleName = prompt("По-батькові");
+console.log (middleName);
+let  lastName = prompt("Прізвище");
+console.log (lastName);
+
