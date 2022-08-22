9aea76a5da8dfbc80628f85d101261c3916475fd
Jaroslavna <kostiv88@icloud.com>
2022-08-22 15:15:05 +0300

    add

---
+++ Homework9/main.js
@@ -0,0 +1,311 @@
+//let simpsons = [
+   // {
+     //   name: 'Bart',
+      //  surname: 'Simpson',
+      //  age: 10,
+      //  info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+      //  photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+   // },
+    //{
+     //   name: 'Homer',
+     //   surname: 'Simpson',
+    //    age: 40,
+       // info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+       // photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+   // },
+  //  {
+      //  name: 'Marge',
+      //  surname: 'Simpson',
+      //  age: 38,
+       // info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+     //   photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+   // },
+  //  {
+      //  name: 'Lisa',
+    //    surname: 'Simpson',
+    //    age: 9,
+   //     info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+   //     photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+  //  },
+  //  {
+     //   name: 'Maggie',
+    //    surname: 'Simpson',
+    //    age: 1,
+   //     info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+   //     photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+  //  },
+//];
+
+//Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+//Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+
+//let target=document.getElementsByClassName('target')[0];
+//for (let simpson of simpsons) {
+   // let div = document.createElement('div');
+    //div.classList.add('member');
+   // let productInfo = document.createElement('h2');
+   // productInfo.innerText = `${simpson.name}
+                            //  ${simpson.surname}
+                            //  ${simpson.age}
+                            //  ${simpson.info}`
+    //div.appendChild(productInfo);
+   // let productImg = document.createElement('img');
+   // productImg.src=simpson.photo;
+   // target.appendChild(div);
+   // target.appendChild(productImg);
+//}
+//let coursesArray = [
+  // {
+      //  title: 'JavaScript Complex',
+      //  monthDuration: 5,
+     //   hourDuration: 909,
+     //   modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+ //   },
+ //   {
+      //  title: 'Java Complex',
+      ///  monthDuration: 6,
+       // hourDuration: 909,
+      //  modules: ['html', 'css', 'js','mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core','java advanced']
+  //  },
+   // {
+     //   title: 'Python Complex',
+      //  monthDuration: 6,
+       // hourDuration: 909,
+      //  modules: ['html',
+          //  'css',
+         //   'js',
+         //   'mysql',
+          //  'mongodb',
+         //  'angular',
+         //   'aws',
+          //  'docker',
+         //   'python core',
+       //     'python advanced']
+   // },
+  //  {
+     //   title: 'QA Complex',
+      //  monthDuration: 4,
+      //  hourDuration: 909,
+       // modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+   // },
+  //  {
+     //   title: 'FullStack',
+       // monthDuration: 7,
+        //hourDuration: 909,
+      //  modules: ['html','css','js','mysql','mongodb','react','angular','aws','docker','git','node.js','python','java']
+  //  },
+   // {
+  //      title: 'Frontend',
+    //    monthDuration: 4,
+   //     hourDuration: 909,
+    //    modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+  //  }
+//];
+
+//Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих
+// властивостей, для властивості modules зробити список з елементами
+//Приклад структири знаходиться у файлі example.png
+
+//let target=document.getElementsByClassName('target')[0];
+//for (let targetElement of coursesArray)
+//{
+  //  let father = document.createElement('div');
+   // father.classList.add('father');
+   // father.style.width = '200px';
+
+   // let top = document.createElement('div');
+
+  //  let tophead = document.createElement('div');
+   // tophead.classList.add('tophead');
+   // tophead.style.textAlign = 'center';
+   // tophead.style.padding = '10px';
+  //  document.createElement('h2');
+   // tophead.innerText = `${targetElement.title}`;
+
+   // let twicediv = document.createElement('div');
+   // twicediv.classList.add('twicediv');
+  //  twicediv.style.display = 'flex';
+   // twicediv.style.justifyContent = 'space-around';
+  //  twicediv.style.padding = '10px';
+
+  //  let twdiv1 = document.createElement('div');
+   // document.createElement('h2');
+  //  twdiv1.innerText = `${targetElement.monthDuration}`;
+
+  //  let twdiv2 = document.createElement('div');
+   // document.createElement('h2');
+  //  twdiv2.innerText = `${targetElement.hourDuration}`;
+
+   // let foot = document.createElement('div');
+    //   for (let i=0; i <= 11; i++)
+    //   {
+       //    let li = document.createElement('li');
+      //     li.innerText = `${targetElement.modules[i]}`;
+      //     foot.appendChild(li);
+
+      // }
+     //   target.appendChild(father);
+     //   father.append(top,foot);
+      //  top.appendChild(tophead);
+     //   top.appendChild(twicediv);
+     //   twicediv.append(twdiv1,twdiv2);
+
+//}
+
+
+//- створити блок,
+// - додати йому класи wrap, collapse, alpha, beta
+//- додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+//- додати цей блок в body.
+//- клонувати його повністю, та додати клон в body.
+
+
+//let body=document.getElementsByTagName('body')[0]
+//let div1 = document.createElement('div');
+
+//div1.classList.add('wrap', 'collapse', 'alpha', 'beta');
+//div1.innerText = ('hello world');
+
+//div1.style.background = 'red';
+//div1.style.fontcolor = 'yellow';
+//div1.style.fontsize = '15px';
+//div1.style.margin = '20px';
+
+//body.appendChild(div1);
+//let clone = div1.cloneNode(true);
+//clone.innerText = ('goodbye world');
+
+//body.appendChild(clone);
+
+
+//- Є масив:
+ //   ['Main','Products','About us','Contacts']
+//Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+//Завдання робити через цикли.
+ //let array = ['Main','Products','About us','Contacts'];
+ //let menu=document.getElementsByClassName('menu')[0]
+  //  let div = document.createElement('div');
+  //  for (let i = 0; i <= array.length; i++)
+//{
+   //     let li = document.createElement('li');
+    //    li.innerText = `${array[i]}`;
+   //     menu.appendChild(li);
+//}
+//let coursesAndDurationArray = [
+   // {title: 'JavaScript Complex', monthDuration: 5},
+  //  {title: 'Java Complex', monthDuration: 6},
+//    {title: 'Python Complex', monthDuration: 6},
+//    {title: 'QA Complex', monthDuration: 4},
+//    {title: 'FullStack', monthDuration: 7},
+  //  {title: 'Frontend', monthDuration: 4}
+//];
+//Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+//Завдання робити через цикли.
+//let target = document.getElementsByClassName('target')[0];
+///for (let item of coursesAndDurationArray)
+//{
+//let div = document.createElement('div');
+//div.innerText = `${item.title} ----- ${item.monthDuration}`;
+//let div2 = document.createElement('div');
+//div2.innerText = `${item.monthDuration}`;
+//target.append (div,div2);
+ //   target.appendChild(div);
+//}
+
+
+//За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
+// в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//  Завдання робити через цикли.
+
+//let target = document.getElementsByClassName('target')[0];
+//for (let targetElement of coursesAndDurationArray)
+//{
+//let div = document.createElement('div');
+//div.classList.add('item');
+//let h1 = document.createElement('h1');
+//h1.classList.add('heading');
+//h1.innerText = `${targetElement.title}`;
+
+//let p = document.createElement('p');
+//p.classList.add('description');
+//p.innerText = `${targetElement.monthDuration}`;
+//div.append(h1,p);
+//target.appendChild(div);
+//}
+
+//Створити довільний елемент з id = text.
+// Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+    //let name = document.getElementById('name')
+     //  name.onclick = function ()
+      //{
+   // let text =  document.getElementById('text')
+     //  text.hidden = true;
+//}
+//- створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+// При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18,
+// та повідомити про це користувача
+
+//let form1=document.forms.form1
+//form1.onsubmit = function (e)
+//{
+   // e.preventDefault();
+ //   let age = this.age.value;
+ //   if (age<18)
+//   {
+  //     alert("Ban on minors")
+ //   }
+ //   else
+//   {
+//        console.log (age);
+//        alert("Entry is allowed")
+//    }
+// }
+
+//*** Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+ //   При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+//(Додатковачастина для завдання)
+let form1 = document.forms.form1
+form1.onsubmit = function (e) {
+    e.preventDefault();
+    let line = this.line.value;
+    let cell = this.cell.value;
+    let info = this.info.value;
+    let items = {line, cell, info};
+    let users = [];
+    users.push(items);
+    console.log(users);
+
+    let target = document.getElementsByClassName('target')[0];
+    target.style.background = 'red'
+    target.style.height = '500px'
+    for (let user of users)
+    {
+        for (let i = 0; i < `${user.line}`; i++)
+        {
+            let div = document.createElement('div');
+            target.appendChild(div);
+
+            div.classList.add ('divclass');
+            let divclass = document.getElementsByClassName('divclass')[0];
+            divclass.style.display = 'flex';
+            divclass.style.justifyContent = 'space-around'; //не розумію, чого спрацьовує лише для першого блоку
+
+            for ( let j = 0; j<`${user.cell}`;j++)
+            {
+                let div2 = document.createElement('div');
+                div.appendChild(div2);
+                let h6 = document.createElement('h6');
+                div2.appendChild(h6);
+                h6.innerText = `${user.info}`;
+
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
